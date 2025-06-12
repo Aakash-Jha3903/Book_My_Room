0c710cbd1f4dc93d03155c9abd055a5944ca6ecd
Aakash <aakashjha343@gmail.com>
2025-06-12 12:28:34 +0530

    register/login & Dashboard for vendor

--- OYO_project/accounts/admin.py
+++ OYO_project/accounts/admin.py
@@ -1,3 +1,6 @@
+#accounts/admin.py
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(HotelUser)
+admin.site.register(HotelVendor)
--- OYO_project/accounts/urls.py
+++ OYO_project/accounts/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
     path('register/' , views.register, name='register'),
     path('send_otp/<email>/' , views.send_otp, name='send_otp'),
     path('verify-otp/<email>/' , views.verify_otp, name='verify_otp'),
-
-
-    path('verify-account/<token>/', views.verify_email_token, name="verify_email_token")
-
+    
+    path('verify-account/<token>/', views.verify_email_token, name="verify_email_token"),
+    
+    path('register-vendor/' , views.register_vendor, name='register_vendor'),
+    path('login-vendor/' , views.login_vendor, name='login_vendor'),
+    
+    path('dashboard/', views.dashboard , name="dashboard"),
+  
 ]
-# OYO_project
